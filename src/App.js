a6e57e1edf100607ab6fdd4f5c67dd648b7de969
rahathossain99 <rhossainrahat@gmail.com>
2024-02-06 13:39:32 +0600

    update router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import Home from './Home';
+import About from './About';
+import Products from './Products';
+import Contact from './Contact';
+import SingleProduct from './SingleProduct';
+import Cart from './Cart';
+
 
 const App = () => {
-  return <div>Welcome to Thapa React E-Commerce Website</div>;
+  return(
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/products" element={<Products/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/singleproduct/:id" element={<SingleProduct/>}/>
+        <Route path="/cart" element={<Cart/>}/>
+
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
