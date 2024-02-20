ca09761c351531528715f596865e22362be83692
rahathossain99 <rhossainrahat@gmail.com>
2024-02-20 14:45:26 +0600

    componentContext.js added

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -1,7 +1,21 @@
-import { createContext } from "react";
+import axios from "axios";
+import { createContext, useEffect } from "react";
 
 const AppContext=createContext();
+
+const API="https://pujakaitem.com/api/products";
+
+
 const AppProvider=({childern})=>{
+    const getProducts=async (url)=>{
+        const res=await axios.get(url);
+        console.log(res);
+        const products=await res.data;
+    }
+
+    useEffect(()=>{
+        getProducts(API);
+    },[]);
     return (<AppContext.Provider>{childern}</AppContext.Provider>)
 }
 export {AppProvider,AppContext};
