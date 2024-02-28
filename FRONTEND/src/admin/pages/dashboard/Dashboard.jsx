e50e4273532394cc001669d96d53e7619557cc28
rahathossain99 <rhossainrahat@gmail.com>
2024-02-28 11:06:20 +0600

    all initail folders are created for admin

---
+++ FRONTEND/src/admin/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './dashboard.scss'
+
+
+const Dashboard = () => {
+  return (
+    <div className='dashboard'>Dashboard</div>
+  )
+}
+
+export default Dashboard;
--- FRONTEND/src/layouts/AdminLayout.js
+++ FRONTEND/src/layouts/AdminLayout.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../admin/pages/login/Login';
+import Dashboard from '../admin/pages/dashboard/Dashboard';
+import List from '../admin/components/list/List';
+import Single from '../admin/components/single/Single';
+import New from '../admin/components/new/New';
 
 const AdminLayout = () => {
   return (
-    <div>hk</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/admin">
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="create" element={<New />} />
+          </Route>
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="create" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default AdminLayout;
