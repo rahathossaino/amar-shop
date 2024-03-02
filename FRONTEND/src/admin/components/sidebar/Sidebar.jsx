e902d9f0b5517071f41f29acb727011429311086
rahathossain99 <rhossainrahat@gmail.com>
2024-03-02 08:42:12 +0600

    datatable has been  implemented

--- FRONTEND/src/admin/components/sidebar/Sidebar.jsx
+++ FRONTEND/src/admin/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './sidebar.scss';
+import {NavLink} from 'react-router-dom';
 import { MdDashboard } from "react-icons/md";
 import { FaUser,FaUserCircle } from "react-icons/fa";
 import { MdProductionQuantityLimits,MdOutlineLogout,MdLocalShipping } from "react-icons/md";
@@ -19,38 +20,54 @@ const sidebar = () => {
             <ul>
               <p className='title'>MAIN</p>
                 <li>
-                  <MdDashboard className='icon'/>
-                  <span>Dashboard</span>
+                  <NavLink to='/admin/dashboard' className="nav-link">
+                    <MdDashboard className='icon'/>
+                    <span>Dashboard</span>
+                  </NavLink>
                 </li>
                 <p className='title'>LIST</p>
                 <li>
-                  <MdProductionQuantityLimits className='icon'/>
-                  <span>Products</span>
+                  <NavLink to='/admin/products' className="nav-link">
+                    <MdProductionQuantityLimits className='icon'/>
+                    <span>Products</span>
+                  </NavLink> 
                 </li>
                 <li>
-                  <BsCreditCardFill className='icon'/>
-                  <span>Orders</span>
+                  <NavLink to='/admin/orders' className="nav-link">
+                      <BsCreditCardFill className='icon'/>
+                      <span>Orders</span>
+                  </NavLink>
                 </li>
                 <li>
-                  <MdLocalShipping className='icon'/>
-                  <span>Delivary</span>
+                  <NavLink to='/admin/delivary' className="nav-link">
+                    <MdLocalShipping className='icon'/>
+                    <span>Delivary</span>
+                  </NavLink>
                 </li>
                 <li>
-                  <FaUser className='icon'/>
-                  <span>User</span>
+                  <NavLink to='/admin/users' className="nav-link">
+                    <FaUser className='icon'/>
+                    <span>Users</span>
+                  </NavLink>
                 </li>
                 <p className='title'>ADMIN</p>
                 <li>
-                  <IoSettings className='icon'/>
-                  <span>Setings</span>
+                  <NavLink to='/admin/settings' className="nav-link">
+                    <IoSettings className='icon'/>
+                    <span>Setings</span>
+                  </NavLink>
                 </li>
                 <li>
-                  <FaUserCircle className='icon'/>
-                  <span>Profile</span>
+                  <NavLink to='/admin/profile' className="nav-link">
+                    <FaUserCircle className='icon'/>
+                    <span>Profile</span>
+                  </NavLink>
                 </li>
                 <li>
-                  <IoLogOut className='icon'/>
-                  <span>Logout</span>
+                 <NavLink to='/admin/logout' className="nav-link">
+                    <IoLogOut className='icon'/>
+                    <span>Logout</span>
+                  </NavLink>
                 </li>
             </ul>
         </div>
